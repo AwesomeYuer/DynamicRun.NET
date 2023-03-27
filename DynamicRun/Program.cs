a4444cbdc2b2ab1ede5c38b67deeeac612c72a46
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-03-27 15:01:53 +0800

    123

--- DynamicRun/Program.cs
+++ DynamicRun/Program.cs
@@ -2,33 +2,89 @@
 using System;
 using System.IO;
 using System.Reactive.Linq;
+namespace DynamicRun;
 
-namespace DynamicRun
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
+        var watcherSourcesDirectoryPath = Path.Combine(Environment.CurrentDirectory, "Sources");
+
+        Console.WriteLine($"Running from: {Environment.CurrentDirectory}");
+        Console.WriteLine($"Sources from: {watcherSourcesDirectoryPath}");
+        Console.WriteLine("Modify the sources to re-compile and run it again!");
+
+        var compiler = new Compiler();
+        var runner = new Runner();
+
+        if (!Directory.Exists(watcherSourcesDirectoryPath))
         {
-            var sourcesPath = Path.Combine(Environment.CurrentDirectory, "Sources");
-
-            Console.WriteLine($"Running from: {Environment.CurrentDirectory}");
-            Console.WriteLine($"Sources from: {sourcesPath}");
-            Console.WriteLine("Modify the sources to compile and run it!");
-
-            var compiler = new Compiler();
-            var runner = new Runner();
-
-            using (var watcher = new ObservableFileSystemWatcher(c => { c.Path = @$".{Path.DirectorySeparatorChar}Sources"; }))
-            {
-                var changes = watcher.Changed.Throttle(TimeSpan.FromSeconds(.5)).Where(c => c.FullPath.EndsWith(@"DynamicProgram.cs")).Select(c => c.FullPath);
-
-                changes.Subscribe(filepath => runner.Execute(compiler.Compile(filepath), new[] { "France" }));
-
-                watcher.Start();
-
-                Console.WriteLine("Press any key to exit!");
-                Console.ReadLine();
-            }
+            Directory.CreateDirectory(watcherSourcesDirectoryPath);
         }
+
+        using var watcher = new ObservableFileSystemWatcher
+                                    (
+                                        (c) =>
+                                        {
+                                            c.Path = watcherSourcesDirectoryPath;
+                                        }
+                                    );
+        var changes = watcher
+                            .Changed
+                            .Throttle
+                                (TimeSpan.FromSeconds(.5))
+                            //.Where
+                            //    (c => c.FullPath.EndsWith(@"DynamicProgram.cs"))
+                            .Select
+                                (c => c.FullPath)
+                            ;
+        changes
+            .Subscribe
+                (
+                    (filePath) =>
+                    {
+                        Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        try
+                        {
+                            Console.WriteLine (filePath);
+                            runner
+                                .Execute
+                                    (
+                                        compiler.Compile(filePath)
+                                        , new[] { "France" }
+                                    );
+                            
+                        }
+                        catch (Exception e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{e}");
+                        }
+                        Console.ResetColor();
+                        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>");
+
+                    }
+                );
+
+        watcher.Start();
+
+        var sourcesDirectoryPath = @"..\..\..\sources";
+
+        var files = Directory
+                        .GetFiles
+                            (
+                                sourcesDirectoryPath
+                                , "*.cs"
+                            );
+
+        foreach (var file in files)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(file);
+            var filePath = Path.Combine(watcherSourcesDirectoryPath, fileName);
+            File.Copy(file, filePath, true);
+        }
+        Console.WriteLine("Press any key to exit!");
+        Console.ReadLine();
     }
 }
--- DynamicRun/Sources/DynamicProgram.cs
+++ DynamicRun/Sources/DynamicProgram.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Reactive.Linq;
+// using Microshaoft;
 
 const string s = $"Hello{" "} World"; // C# 10 Constant Interpolated Strings
-var hello = $"{s} from {args[0]}!";
+var hello = $"{s} from {DateTime.Now}!";
     
 Console.WriteLine(hello);
+Console.WriteLine("1111111111111");
